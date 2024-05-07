f55b5f7543f88a7405c3f636452e5f3035e32f11
yago <yagosonicmassaroni@gmail.com>
2024-05-07 14:03:42 -0300

    :lipstick: tipagem de temas styled components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -16,7 +16,7 @@ const buttonVariants = {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-
+  border-radius: 4px;
   background-color: ${props => props.theme.primary};
   /*${props => {
    return css`
