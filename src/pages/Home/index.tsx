d445be4c7bfad6f2d8dd87f2bed2318b8a3dae42
yago <yagosonicmassaroni@gmail.com>
2024-05-09 16:47:46 -0300

    :lipstick: validando formularios

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react"
 import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as zod from "zod"
 import {
   CountdownContainer,
   HomeContainer,
@@ -10,9 +12,19 @@ import {
   MinutesAmountInput,
 } from "./styles"
 
+  const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod
+    .number()
+    .min(5, "o ciclo precisa ser de no mínimo de 5 minutos")
+    .max(60, "o Ciclo precisa ser de no máximo de 60 minutos")
+  })
+
 export function Home() {
 
-  const {register, handleSubmit, watch} = useForm()
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  })
 
   function handleCreateNewCycle(data: any) {
     console.log(data)
