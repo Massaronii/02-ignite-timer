fbbdf6aff612432cc562723af6b6d9eae6a46259
yago <yagosonicmassaroni@gmail.com>
2024-05-11 17:41:27 -0300

    :sparkles: mudando o titulo da pagina pelo tempo do contador

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -52,12 +52,18 @@ export function Home() {
    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
+
     if (activeCycle) {
-      setInterval(() => {
+       interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate)
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -71,6 +77,7 @@ export function Home() {
 
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -84,8 +91,13 @@ export function Home() {
   const minutes = String(minutesAmount).padStart(2, "0")
   const seconds = String(secondsAmount).padStart(2, "0")
 
+  useEffect(() => {
+    if(activeCycle) document.title = `${minutes}:${seconds}`
+
+  }, [minutes, seconds, activeCycle])
+
   const task = watch("task")
-  const isSubmitDisabled = task
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
@@ -122,13 +134,13 @@ export function Home() {
 
         <CountdownContainer>
           <span>{minutes[0]}</span>
-          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
           <Separator>:</Separator>
           <span>{seconds[0]}</span>
           <span>{seconds[1]}</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled={!isSubmitDisabled} type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
